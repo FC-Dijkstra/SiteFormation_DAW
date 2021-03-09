3c809a856dff305714a2c716cecbaf78b6ead030
albotw <yann.trou@gmail.com>
2021-03-09 10:12:53 +0100

    altération de QCM + ajout diagrammes
    modification de SCM pour paser en MVC full PHP
    Ajout de diagrammes pour le Use Case level1 et la BDD

---
+++ front/PHP/QCMView.php
@@ -0,0 +1,5 @@
+<?php $title = "Affichage QCM"; ?>
+
+
+<?php ob_start(); ?>
+
--- front/PHP/template.php
+++ front/PHP/template.php
@@ -2,11 +2,13 @@
 
 <head>
     <meta charset="utf-8" />
+    <title><?= $title ?></title>
 </head>
 
 <body>
     <div id="output"></div>
 
+    <?= $content ?>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js" defer></script>
     <script src="../JS/QCM.js" defer></script>
 </body>
