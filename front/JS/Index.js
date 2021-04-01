a04c67ca778b98aa9493653b14080cd9d326bf45
Aysao <47144815+Aysao@users.noreply.github.com>
2021-04-01 11:47:36 +0200

    Add files via upload

--- front/JS/Index.js
+++ front/JS/Index.js
@@ -1,31 +1,60 @@
 var wheel = 300;
+var flaganime = true;
 function onScrollHandler(e)
 {
 	var newImageUrl = $("#slide").attr("src");
-	wheel = wheel + e.originalEvent.deltaY;
-	console.log(wheel);
-	console.log(Math.abs(wheel % 3));
-	if(wheel < 0)
-		wheel = wheel + 300;
-	if (Math.abs(wheel % 3) == 0 ) {
-		newImageUrl = "img1.jpg"
+	if ($("#slide").is(":animated") || $("#slide2").is(":animated"))
+	{
+		console.log("en animation ...");
 	}
-	if (Math.abs(wheel % 3) == 2) {
-		newImageUrl = "img2.jpg"
+	else
+	{
+		wheel = wheel + (e.originalEvent.deltaY);
+		if(wheel < 0)
+			wheel = wheel + 300;
+		if (Math.abs(wheel % 3) == 0 ) {
+			newImageUrl = "img1.jpg";
+		}
+		if (Math.abs(wheel % 3) == 2) {
+			newImageUrl = "img2.jpg";
+		}
+		if (Math.abs(wheel % 3) == 1)  {
+			newImageUrl = "img3.jpg";
+		}
+		
+		if($("#slide").css("z-index") == 2 && flaganime == true)
+		{
+			flaganime = false;
+			$("#slide").attr("src",newImageUrl);
+			$("#slide2").animate({left:'50px',top:'-100%'});
+			$("#slide").css("z-index","1");
+			$("#slide2").css("z-index","2");
+			$("#slide").css("top","0px");
+			$("#slide").css("left","0px");
+			$("#slide2").css("top","0px");
+			$("#slide2").css("left","0px");
+			flaganime = true;
+			
+		}
+		else if(flaganime == true)
+		{
+			flaganime = false;
+			$("#slide2").attr("src",newImageUrl);
+			$("#slide").animate({left:'50px', top:'-100%'});
+			$("#slide2").css("z-index","1");
+			$("#slide").css("z-index","2");
+			$("#slide").css("top","0px");
+			$("#slide").css("left","0px");
+			$("#slide2").css("top","0px");
+			$("#slide2").css("left","0px");
+			flaganime = true;
+		}
 	}
-	if (Math.abs(wheel % 3) == 1)  {
-		// deux balise img, l'une au dessus de l'autre celle en front fait l'animation
-		// et passe ensuite derriere la deuxième, vise versa (utilisé z-index : 1 ou -1) . 
-		newImageUrl = "img3.jpg"
-	}
-	
-	
-	$("#slide").attr("src",newImageUrl);
 };
 
 function init()
 {
-	$("#slide").on("wheel",function(e){
+	$("img").on("wheel",function(e){
 		onScrollHandler(e);
 		});
 }
