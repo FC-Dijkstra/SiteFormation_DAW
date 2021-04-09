474a0e9b972e02bb6b2bb2c0776f43206f86a838
Gwen <gwendal-lottin@hotmail.fr>
2021-04-09 12:16:43 +0200

    Not the same id baka UwU

--- front/PHP/Cours/web.php
+++ front/PHP/Cours/web.php
@@ -4,9 +4,9 @@
 include_once($_SERVER['DOCUMENT_ROOT']."/back/class/cours.php");
 $liste = array();
 array_push($liste, new cours(1, "Apprenez HTML", "EZ", -1, 1));
-array_push($liste, new cours(1, "Allez plus loin avec HTML", "EZ", -1, 1));
-array_push($liste, new cours(1, "Apprenez CSS", "WANNA KMS", -1, 2));
-array_push($liste, new cours(1, "Apprenez JS", "EZ", -1, 3));
+array_push($liste, new cours(2, "Allez plus loin avec HTML", "EZ", -1, 1));
+array_push($liste, new cours(3, "Apprenez CSS", "WANNA KMS", -1, 2));
+array_push($liste, new cours(4, "Apprenez JS", "EZ", -1, 3));
 ?>
 
     <?php include ('../../Header_Footer/template.php') ?>
