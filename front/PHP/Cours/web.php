76588735ea709618b3809618ea2abf8d0f46c1fd
Gwen <gwendal-lottin@hotmail.fr>
2021-04-09 12:14:49 +0200

    Sample array

--- front/PHP/Cours/web.php
+++ front/PHP/Cours/web.php
@@ -1,6 +1,13 @@
 <!DOCTYPE html>
 <html lang="fr" >
-
+<?php
+include_once($_SERVER['DOCUMENT_ROOT']."/back/class/cours.php");
+$liste = array();
+array_push($liste, new cours(1, "Apprenez HTML", "EZ", -1, 1));
+array_push($liste, new cours(1, "Allez plus loin avec HTML", "EZ", -1, 1));
+array_push($liste, new cours(1, "Apprenez CSS", "WANNA KMS", -1, 2));
+array_push($liste, new cours(1, "Apprenez JS", "EZ", -1, 3));
+?>
 
     <?php include ('../../Header_Footer/template.php') ?>
     <body>
