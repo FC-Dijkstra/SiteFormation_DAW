dc11c9265d608b958572a7807071dc234355d7c4
albotw <yann.trou@gmail.com>
2021-04-12 13:30:36 +0200

    début implémentation recommandations
    ajout des appels SQL
    ajout d'un fichier de test

---
+++ back/controllers/recommandation.php
@@ -0,0 +1,21 @@
+<?php
+require_once (__DIR__ . "./../helpers/db.php");
+require_once (__DIR__ . "./../helpers/print.php");
+function recommandations()
+{
+    $id = 2;
+    $result = db::getInstance()->query("SELECT categorie, COUNT(*) as cpt FROM cours WHERE id IN (SELECT cours FROM abonnements WHERE user = {$id}) GROUP BY categorie ORDER BY cpt DESC LIMIT 3", []);
+    var_dump($result);
+    println();
+
+    for($i = 0; $i < 3; $i++)
+    {
+        $categorie =  $result[$i]["categorie"];
+        $idCours = db::getInstance()->query("SELECT * FROM cours WHERE id NOT IN (SELECT cours FROM abonnements WHERE user = {$id}) AND categorie = {$categorie} LIMIT 1", []);
+        var_dump($idCours);
+        println();
+    }
+
+
+
+}
