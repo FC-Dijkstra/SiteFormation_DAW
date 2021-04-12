7b9295b242ba4bea2afc1391358467cb574cdbed
Hahiro <34438643+PyHahiro@users.noreply.github.com>
2021-04-12 14:18:45 +0200

    Copy web dans app

--- front/PHP/Cours/application.php
+++ front/PHP/Cours/application.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
+<link rel="stylesheet" href="../../CSS/web.css" type="text/css"/>
 <?php
 include_once($_SERVER['DOCUMENT_ROOT']."/back/class/cours.php");
 include($_SERVER['DOCUMENT_ROOT']."/front/Header_Footer/template.php");
@@ -10,6 +11,25 @@ array_push($liste, new cours(3, "Apprenez CSS", "WANNA KMS", -1, 2));
 array_push($liste, new cours(4, "Apprenez JS", "EZ", -1, 3));
 
 ?>
-
+ <body>
+        <div class="html">
+            <h4 class="langage">HTML</h4>
+            <div class="cours">
+                <img class="box1" src="/front/IMG/girl.png">
+                <h3 class="box2"> Les bases du html</h3>
+                <p class="box3">Facile ★✩✩ </p>
+                <p class="box4"> Ceci est une description</p>
+            </div>
+        </div>
+        <div class="CSS">
+            <h4 class="langage">CSS</h4>
+        </div>
+        <div class="JS">
+            <h4 class="langage">JAVASCRIPT</h4>
+        </div>
+        <div class="PHP">
+            <h4 class="langage">PHP</h4>
+        </div>
+    </body>
 <?php
 include($_SERVER['DOCUMENT_ROOT']."/front/Header_Footer/footer.php");
