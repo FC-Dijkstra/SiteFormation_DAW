195fdd4770432677ee0810c702ec40898c60c730
Hahiro <34438643+PyHahiro@users.noreply.github.com>
2021-04-12 18:09:47 +0200

    Changement du jour
    Header up (WIP)
    +darkmode

--- front/Header_Footer/header.php
+++ front/Header_Footer/header.php
@@ -7,19 +7,21 @@
 
 <header>
     <div class="header">
-        <img class="logo" src="/front/IMG/logo.png" alt="Logo" />
-        <ul class="menu">
+        <img class="hbox1" src="/front/IMG/logo.png" alt="Logo" />
+        <ul class="hbox2">
             <li class="menu-item"><a href="/front/PHP/accueil.php">Accueil</a></li>
             <li class="menu-item"><a href="/front/PHP/cours.php">Cours</a></li>
             <li class="menu-item"><a href="/front/PHP/forum.php">Forum</a></li>
-            <li class="menu-item">
-                <ul class="submenu">
-                    <li class="menu-item connexion"><a href="#">CONNEXION</a></li>
-                    <li class="menu-item inscription"><a href="#">INSCRIPTION</a></li>
-                    <li class="menu-item darkmode"><button onclick="toggleTheme()" type="button">Dark Mode</button></li>
-                </ul>
-            </li>
+            <div class="htrait">
+            </div>
         </ul>
+        
+        <ul class="hbox3">
+            <li class="menu-item connexion"><a href="#">Connexion</a></li>
+            <li class="menu-item inscription"><a href="#">Inscription</a></li>
+            <li class="menu-item darkmode"><button onclick="toggleTheme()" type="button">Dark Mode</button></li>
+        </ul>
+       
     </div>
     <?= isset($content)?$content:"" ?>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
--- front/JS/Sombre.js
+++ front/JS/Sombre.js
@@ -1,9 +1,18 @@
 function toggleTheme()
 {
-    var theme = document.getElementsByTagName('link')[0];
-    if (theme.getAttribute('href') == '/front/CSS/menu.css') {
-        theme.setAttribute('href', '/front/CSS/DarkTheme/dark.css');
-    } else {
-        theme.setAttribute('href', '/front/CSS/menu.css');
-    }
+    var header = document.getElementsByClassName("header")[0];
+    var footer = document.getElementsByClassName("footer")[0];
+    var body = document.getElementsByTagName("body")[0];
+    toggleElement(header);
+    toggleElement(footer);
+    toggleElement(body);
 }
+
+function toggleElement(e) {
+    if(e.className.includes("dark"))
+      e.className = e.className.slice(0, e.className.indexOf('dark')) + 'light';
+    else if(e.className.includes("light"))
+      e.className = e.className.slice(0, e.className.indexOf('light')) + 'dark';
+    else 
+      e.className+= ' dark';
+  }
