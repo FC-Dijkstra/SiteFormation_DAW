4ac464848cb8d846ea34f5d68ad80ae6ec18750b
Hahiro <34438643+PyHahiro@users.noreply.github.com>
2021-04-16 16:44:58 +0200

    Pull avancement profil

--- front/PHP/Utilisateur/profil.php
+++ front/PHP/Utilisateur/profil.php
@@ -1,34 +1,46 @@
 <!DOCTYPE html>
 <html lang="fr" >
 <link rel="stylesheet" href="../../CSS/Utilisateur/profil.css" type="text/css"/>
-    <?php include_once($_SERVER['DOCUMENT_ROOT']."/front/Header_Footer/header.php"); ?>
-    
+<?php include_once($_SERVER['DOCUMENT_ROOT']."/front/Header_Footer/header.php");
+?>
+
     <body>
         <div id="profil">
-        <h2> Mon profil </h2>
+        <h1> Mon profil </h1>
             <div id="pbox1">
                 <img id="photoprofil" src="/front/IMG/defaut_profil.png" alt="profil" />
             </div>
             <div id="pbox2">
+            <form id="modifprofil">
                 <label id="nom" type="text">Nom :</label>
                 <input type="text" id="info_nom" value="Martin" readonly><br>
 
-                <label class="prenom" type="text">Prénom :</label>
+                <label id="prenom" type="text">Prénom :</label>
                 <input type="text" id="info_prenom" value="Jonathan" readonly><br>
 
-                <label class="mail" type="text">Adresse mail liée:</label>
-                <input type="email" id="info_mail" value="jonathan.martinmaestre71@gmail.com" readonly><br>
+                <label id="mail" type="text">Adresse mail liée:</label>
+                <input type="email" id="info_mail" value="jonathan.martinmaestre71@gmail.com" readonly><br><br>
 
-                <a class="mdp">Changer mot de passe </a><br>
+                <a id="mdp">Changer mot de passe </a><br>
 
+                <label class="mdp" type="text">Nouveau mot de passe</label>
+                <input class="mdp" type=text id="info_nouveaumdp" value=""><br>
+
+                <label class="mdp" type="text">Confirmation nouveau mot de passe</label>
+                <input class="mdp" type=text id="info_nouveaumdp" value=""><br>
+
+                <input type="submit" id="enregistrer" value="Enregistrer">
                 <button id="modifier" type="button" onclick="ModifierInfos()" >Modifier </button>
+                
+            <form>
             </div>
         </div>
 
-        <hr>
+        
 
         <div id="cours">
             <h1> Cours suivis : </h1>
+            <hr>
             </br>
             <div class="cours">
                 <img class="box1" src="/front/IMG/girl.png">
@@ -38,14 +50,15 @@
             </div>
         </div>
 
-        <hr>
+        
         <div id="recommandations">
             <h1> Mes recommandations : </h1>
+            <hr>
             </br>
             <div class="reco">
                 <img src="/front/IMG/girl.png">
                 <h3> Les bases du html</h3>
-                <p>Difficulté du cours : facile ★✩✩ </p>
+                <p>facile ★✩✩</p>
             </div>
         </div>
     </body>
