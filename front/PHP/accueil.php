ae67056a5a0d0fb46ad0deb0bb7a943e254307ea
Camille <Camille.dardoize.romsie@gmail.com>
2021-04-21 16:10:42 +0200

    JE PUSH

--- front/PHP/accueil.php
+++ front/PHP/accueil.php
@@ -3,30 +3,35 @@
 <?php include_once ('../Header_Footer/header.php'); ?>
 <link rel="stylesheet" href="../CSS/accueil.css" type="text/css"/>
 <body>
-<h2> Bienvenue sur notre site de cours en ligne ! </h2>
+<h2 id="bvn"> Bienvenue sur notre site de cours en ligne ! </h2>
 <hr>
 <div id="debut">
   <img src="../IMG/logo.PNG">
-  <h3> Apprenez à plusieurs !
+  <h3> Apprenez à plusieurs !</h3>
   <p> Rejoignez notre communauté. Les enseignants postent des cours très souvent et les élèves peuvent s'aider au besoin dans notre forum. </p>
 </div>
 
 <div id="presentation">
-  <div id="droite">
-    <img src="../IMG/logo.PNG">
-    <h3> Des enseignants là pour vous apprendre à devenir des professionnels
+  <div id="gauche">
+    <img src="../IMG/enseignants.PNG">
+    <h3> Des enseignants là pour vous apprendre le meilleur</h3>
     <p> Nous nous efforçons à vous proposer un contenu ludique, complet et formateur. Nous, les enseignants, sommes qualifiés, nous vous écrivons donc des cours digne de confiance, pour vous apprendre à développer le plus rapidement possible. </p>
   </div>
-  <div id="gauche">
-    <img src="../IMG/logo.PNG">
-    <h3> Apprenez depuis votre canapé !
+  <div id="milieu">
+    <img src="../IMG/canape.PNG">
+    <h3> Apprenez depuis votre canapé !</h3>
     <p> Apprenez différents langages de programmation (développement web, développement d'application...) gratuitement et ceux, depuis chez vous !</p>
+  </div>
+  <div id="droite">
+    <img src="../IMG/pro.PNG">
+    <h3> Débutant, amateur ou expert ?</h3>
+    <p> Nous donnons des cours pour tout type de niveau ! Et si vous ne savez pas quel niveau vous avez, faites nos qcm pour avoir des recommandations !</p>
   </div>
 </div>
 
 <hr>
 <div id="cours">
-  <h2> Différentes catégories de cours sont disponibles </h2>
+  <h3> Différentes catégories de cours sont disponibles </h3>
   <a href="/front/PHP/Cours/web.php">
   <img class="hbox1" src="/front/IMG/logo.png"/></a>
   <a href="/front/PHP/Cours/application.php">
@@ -35,7 +40,7 @@
 
 <hr>
 <div id="forum">
-  <h2> Venez parler avec les autres étudiants sur nos forums !</h2>
+  <h3> Venez parler avec les autres étudiants sur nos forums !</h3>
   <input id="bouton" type="button" value="Forum">
 </div>
 
