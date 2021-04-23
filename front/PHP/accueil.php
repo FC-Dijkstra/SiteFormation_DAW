357788b07c61d7e95346bfee936a13700cbe3a7d
Camille <Camille.dardoize.romsie@gmail.com>
2021-04-23 18:45:08 +0200

    J'ai pimpé comme jaja l'accueil mama

--- front/PHP/accueil.php
+++ front/PHP/accueil.php
@@ -3,25 +3,25 @@
 <h2 id="bvn"> Bienvenue sur notre site de cours en ligne ! </h2>
 <hr>
 <div id="debut">
-  <img src="/front/IMG/logo.PNG">
+  <img src="/front/IMG/logo.PNG"/>
   <h3> Apprenez à plusieurs !</h3>
   <p> Rejoignez notre communauté. Les enseignants postent des cours très souvent et les élèves peuvent s'aider au besoin dans notre forum. </p>
 </div>
 
 <div id="presentation">
   <div id="gauche">
-    <img src="/front/IMG/enseignants.PNG">
-    <h3> Des enseignants là pour vous apprendre le meilleur</h3>
-    <p> Nous nous efforçons à vous proposer un contenu ludique, complet et formateur. Nous, les enseignants, sommes qualifiés, nous vous écrivons donc des cours digne de confiance, pour vous apprendre à développer le plus rapidement possible. </p>
+    <img src="/front/IMG/enseignants.PNG"/>
+    <h3> Là pour vous apprendre le meilleur</h3>
+    <p> Nous nous efforçons à vous proposer un contenu ludique, complet et formateur pour vous apprendre à développer le plus rapidement possible. </p>
   </div>
   <div id="milieu">
-    <img src="/front/IMG/canape.PNG">
-    <h3> Apprenez depuis votre canapé !</h3>
+    <img src="/front/IMG/canape.PNG"/>
+    <h3> Venez apprendre depuis votre canapé !</h3>
     <p> Apprenez différents langages de programmation (développement web, développement d'application...) gratuitement et ceux, depuis chez vous !</p>
   </div>
   <div id="droite">
-    <img src="/front/IMG/pro.PNG">
-    <h3> Débutant, amateur ou expert ?</h3>
+    <img src="/front/IMG/pro.PNG"/>
+    <h3> Vous êtes débutant, amateur ou expert ?</h3>
     <p> Nous donnons des cours pour tout type de niveau ! Et si vous ne savez pas quel niveau vous avez, faites nos qcm pour avoir des recommandations !</p>
   </div>
 </div>
@@ -29,16 +29,23 @@
 <hr>
 <div id="cours">
   <h3> Différentes catégories de cours sont disponibles </h3>
-  <a href="index.php?page=coursWeb">
-  <img class="hbox1" src="/front/IMG/logo.png"/></a>
-  <a href="index.php?page=coursApplication">
-  <img class="hbox1" src="/front/IMG/logo.png"/></a>
+  <a id="a1" href="index.php?page=coursWeb">
+    <img class="hbox1" src="/front/IMG/cours_Site2.png"/>
+    <p> Développement web </p>
+  </a>
+  <a id="a2" href="index.php?page=coursApplication">
+    <img class="hbox1" src="/front/IMG/cours_Site.png"/>
+    <p> Développement d'application </p>
+  </a>
 </div>
 
 <hr>
 <div id="forum">
   <h3> Venez parler avec les autres étudiants sur nos forums !</h3>
-  <input id="bouton" type="button" value="Forum">
+  <a href="index.php?page=accueilForum">
+    <img class="hbox1" src="/front/IMG/Forum.png"/>
+    <p> Forum </p>
+  </a>
 </div>
 
 </body>
