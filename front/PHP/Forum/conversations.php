ec6902d1d00e40ac2735a45ed264cefb8c7ef4c0
albotw <16798879+albotw@users.noreply.github.com>
2021-05-10 16:59:06 +0200

    ajout protections conversation

--- front/PHP/Forum/conversations.php
+++ front/PHP/Forum/conversations.php
@@ -1,10 +1,26 @@
 <link rel="stylesheet" href="/front/CSS/Forum/index.css" type="text/css">
 <?php
-include_once($_SERVER['DOCUMENT_ROOT'] . "/back/class/categorie.php");
-include_once($_SERVER['DOCUMENT_ROOT'] . "/back/controllers/Forum.php");
-$id = $_GET['id'];
-$conversations = recupConvById($id);
-$cateName = categorie::getById($id);
+require_once($_SERVER['DOCUMENT_ROOT'] . "/back/class/categorie.php");
+require_once($_SERVER['DOCUMENT_ROOT'] . "/back/controllers/Forum.php");
+
+
+if (Input::exists())
+{
+    $id = filter_var(Input::get("id"), FILTER_SANITIZE_NUMBER_INT);
+    if (isset($id))
+    {
+        $conversations = recupConvById($id);
+        $cateName = categorie::getById($id);
+    }
+    else
+    {
+        redirect::to("accueil", "Erreur, conversation introuvables");
+    }
+}
+else
+{
+    redirect::to("accueil", "Erreur");
+}
 ?>
 <body>
 <div class="container">
